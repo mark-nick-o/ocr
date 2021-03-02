3718fa9992c79efdec4aef2cea89e14328a2b61f
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-03-02 18:58:41 +0000

    Update read_label_ocr.sh
    check for an email or web address in german before the translate engine - it might be wanted in other languages too ..... in reality (not found in this robot challenge - my pictures !)

--- robot/read_label_ocr.sh
+++ robot/read_label_ocr.sh
@@ -7,6 +7,12 @@
 # if you need co-ordinates in the image for a text you can use hocr option 
 # with tesseract to give an xml output called .hocr
 #
+[ $# -eq 0 ] && { echo "Usage: $0 filename <option: language jpn, spa, fra, deu, ita, rus alternate engine : ocrad >"; exit 1; }
+
+HOME=/home/mark/pics
+NOTWITHAT=0
+NOTWITHDOT=0
+NUMCHAR=0
 
 case $2 in
 
@@ -56,9 +62,40 @@ case $2 in
     [ "$numFields" -eq 2 ] && [ -f /mnt/c/linuxmirror/$1 ] && tesseract /mnt/c/linuxmirror/$1 $1-deu -l deu
     # use dict to translate - noticed you might have joined words which skipped trans engine so did a grep for :- (word contains "milch")
     [ -f $1-deu.txt ] && dict -d fd-deu-eng `cat $1-deu.txt` 
+    #
+    # ---- here im making correction for dict --------------------------
+    #      using rules and linux
+    #
+    # we needed to eliminate office@naturjoghurt.at
+    # because it is a email and not a product
+    # likewise a web address needs eliminating such as this
+    # www.fruchtjoghurt.de
+    # because we can get partial we just say never a @ or . but we could say
+    # @ and a . or 2 . makes it true
+    # we check if we didnt get a @ or a . in that word before we ensure it is checked for yoghurt
+    #
+    # this would work for normal documents, but because it is a stream
+    # which iterates as we read from the ocr at different angles of picture
+    # we need to consider one pass might give us DNATURJOGHURT_
+    # for example therefore we set a turn it off eliminate flag if we get the 
+    # email seen.
+    #
+    [ -f $1-deu.txt ] && cat $1-deu.txt | tr [:lower:] [:upper:] | grep JOGHURT > $HOME/foundWord.txt   # dict missed it for example fruchtjoghurt was not known
+    [ -s $HOME/foundWord.txt ] && NUMCHAR=`wc -c $HOME/foundWord.txt | awk '{ print $1 }'`
+    if [ "$NUMCHAR" -gt 6 ]                                             # it wasnt a cariage return (was at least 6 chars long)
+    then
+        NOTWITHAT=`cat $HOME/foundWord.txt | awk '-F@' '{print NF}'`
+        NOTWITHDOT=`cat $HOME/foundWord.txt | awk '-F.' '{print NF}'`
+        #if !([ "$NOTWITHAT" -eq 2 ] && [ "$NOTWITHDOT" -eq 2 ]) ; then echo "Yogurt" ; fi  # not @ and a . in match word (might mean a email or webaddress)
+        if [ "$NOTWITHAT" -eq 1 ] && [ "$NOTWITHDOT" -eq 1 ] ; then echo "Yogurt" ; else echo "RemovYog" ; cp $HOME/foundWord.txt $HOME/this.txt ; fi      # not @ or a . (might mean a email or webaddress)
+        rm $HOME/foundWord.txt
+    else
+        NUMCHAR=0
+    fi
     [ -f $1-deu.txt ] && cat $1-deu.txt | tr [:lower:] [:upper:] | grep VOLLMILCH && [ $? -eq 0 ] && echo "Whole Milk"   # dict wrongly says unskimmed ? okay if voll milch
     [ -f $1-deu.txt ] && cat $1-deu.txt | tr [:lower:] [:upper:] | grep ROHMILCH && [ $? -eq 0 ] && echo "Raw Milk" && rm $1-deu.txt && exit # dict missed it
-    [ -f $1-deu.txt ] && cat $1-deu.txt | tr [:lower:] [:upper:] | grep MILCH && [ $? -eq 0 ] && echo Milk && rm $1-deu.txt
+    [ -f $1-deu.txt ] && cat $1-deu.txt | tr [:lower:] [:upper:] | grep MILCH && [ $? -eq 0 ] && echo Milk 
+    [ -f $1-deu.txt ] && rm $1-deu.txt
     # could also use trans (look below if you want to choose the translator engine default:google requires net connection)
     # [ -f $1-deu.txt ] && trans de: `cat $1-deu.txt` && rm $1-deu.txt
     ;;
@@ -89,9 +126,12 @@ case $2 in
     # could also use trans (look below if you want to choose the translator engine) however returns shooter for молоко(milk)
     # [ -f $1-rus.txt ] && trans ru: `cat $1-rus.txt` && rm $1-rus.txt
     #
-    # if you have a paid yandex account
+    # if you have a paid yandex account you can use it.
     #
     # /home/mark/pics/yatranslate_notify.sh `cat $1-rus.txt`
+    #
+    # put some basic words in the python code so just translate to lower case 
+    #
     [ -f $1-rus.txt ] && cat $1-rus.txt && rm $1-rus.txt
     ;;
 
