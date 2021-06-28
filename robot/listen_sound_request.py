440eb454c272f51447337991f1d98b5d96b7eb76
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-06-28 18:24:00 +0100

    Update listen_sound_request.py
    added each speech engine supported and also to call our japan MIMI rest API for asr and nict-asr

--- robot/listen_sound_request.py
+++ robot/listen_sound_request.py
@@ -14,6 +14,12 @@ import speech_recognition as sr
 # if the speech recognition using the google engine fails we revert to use the 
 # mimi text to speech engine or kaldi (TO BE COMPLETED)
 #
+# Also needs to cycle every possible (used language) to find out what was said to the robot
+# (TO BE COMPLETED) - Maybe best way is to have a version for each language and then call each one
+# that way we can try to best guess in an order what language it is based on previous questions
+# with last messages being most relevent. (therefore PASS the language and choose the engine accordingly)
+#
+
 if __name__ == "__main__":
 
     if (len(sys.argv) - 1) <= 0:
@@ -22,6 +28,8 @@ if __name__ == "__main__":
        
     r = sr.Recognizer()                                                 # initialize the recognizer
     progHomeMimi = '/home/mark/pics/mimi_trans_sound.sh '
+    recFilePath = '/mnt/c/linuxmirror/'
+    recFileSave = 'recordedSample.wav'
     
     if (len(sys.argv) - 1) == 2:                                        # passed -MIC <duration>
         with sr.Microphone() as source:
@@ -32,7 +40,42 @@ if __name__ == "__main__":
             text = r.recognize_google(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
             print("Decoded Text : {}".format(text))			
         except Exception as ex:
-            print(ex) 
+            print(ex)
+            try:
+                text = r.recognize_bing(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                print("Decoded Text : {}".format(text))			
+            except Exception as ex:
+                print(ex)
+                try:
+                    text = r.recognize_google_cloud(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                    print("Decoded Text : {}".format(text))			
+                except Exception as ex:
+                    print(ex)
+                    try:
+                        text = r.recognize_houndify(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                        print("Decoded Text : {}".format(text))			
+                    except Exception as ex:
+                        print(ex)
+                        try:
+                            text = r.recognize_ibm(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                            print("Decoded Text : {}".format(text))			
+                        except Exception as ex:
+                            print(ex)
+                            try:
+                                text = r.recognize_sphinx(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                                print("Decoded Text : {}".format(text))			
+                            except Exception as ex:
+                                print(ex)
+                                try:
+                                    text = r.recognize_wit(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                                    print("Decoded Text : {}".format(text))			
+                                except Exception as ex:
+                                    print(ex)
+                                    recFilePathFile = recFilePath + recFileSave
+                                    with open(recFilePathFile, "wb") as f:
+                                    f.write(audio.get_wav_data())                           # CHECK IF WE NEED TO DOWNSAMPLE here !!!
+                                    cmd = progHomeMimi + recFileSave                        # run the mimi translator on the input file to see if we can get result (use each possible engine)
+                                    f = os.popen(cmd) 
     else:  
         # ------- read in the requested sound file ---------
         fileNam = "/mnt/c/linuxmirror/" + sys.argv[1]
@@ -48,10 +91,39 @@ if __name__ == "__main__":
             text = r.recognize_google(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
             print("Decoded Text : {}".format(text))			
         except Exception as ex:
-            print(ex) 
-            cmd = progHomeMimi + fileNam                                # run the mimi translator on the input file to see if we can get result (use each possible engine)
-            f = os.popen(cmd) 
+            print(ex)
+            try:
+                text = r.recognize_bing(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                print("Decoded Text : {}".format(text))			
+            except Exception as ex:
+                print(ex)
+                try:
+                    text = r.recognize_google_cloud(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                    print("Decoded Text : {}".format(text))			
+                except Exception as ex:
+                    print(ex)
+                    try:
+                        text = r.recognize_houndify(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                        print("Decoded Text : {}".format(text))			
+                    except Exception as ex:
+                        print(ex)
+                        try:
+                            text = r.recognize_ibm(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                            print("Decoded Text : {}".format(text))			
+                        except Exception as ex:
+                            print(ex)
+                            try:
+                                text = r.recognize_sphinx(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                                print("Decoded Text : {}".format(text))			
+                            except Exception as ex:
+                                print(ex)
+                                try:
+                                    text = r.recognize_wit(audio_data,language="en-US")      # recognize (convert from speech to text) - english USA 
+                                    print("Decoded Text : {}".format(text))			
+                                except Exception as ex:
+                                    print(ex)  
+                                    cmd = progHomeMimi + fileNam                                # run the mimi translator on the input file to see if we can get result (use each possible engine)
+                                    f = os.popen(cmd) 
             
-    #text = r.recognize_google(audio_data,language="en-US")              # recognize (convert from speech to text) - english USA            
     #text = r.recognize_google(audio_data,language="es-ES")              # recognize (convert from speech to text) - spanish
     print(text)
